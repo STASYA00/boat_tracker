7f78597821ee7ff6f2053e4046e0d87110d7749b
STASYA00 <0.0stasya@gmail.com>
2023-02-05 15:32:12 +0100

    change main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,35 @@
-from tracker import *
+import argparse
+import textwrap
+
+from pipeline import Yolo5DeepSortPipeline
 
 if __name__ == "__main__":
-    print("run")
-    TrackerFactory().get(TRACKERS.MOSSE)()
-    t = Tracker()
+    parser = argparse.ArgumentParser(
+			formatter_class=argparse.RawDescriptionHelpFormatter,
+			description=textwrap.dedent('''\
+			USAGE: python3 main.py --source 1.mp4
+
+	        ------------------------------------------------------------------------
+	
+	        Boat detection app combining Yolo object detection and DeepSort tracking.
+            Writes out a video file with the result of the detection.
+	
+	        ------------------------------------------------------------------------
+	
+	        --source        video file for boat detection, str
+	
+	        ------------------------------------------------------------------------
+	
+	        '''), epilog=textwrap.dedent('''\
+	        The algorithm will be updated.
+	        '''))
+            
+    parser.add_argument('--source', type=str, help='path to the video file'
+                                                    ', str')
+
+	############################################################################
+     
+    args = parser.parse_args()
+    
+    pipe = Yolo5DeepSortPipeline(args.source, write=True, show=False)
+    pipe.run()
